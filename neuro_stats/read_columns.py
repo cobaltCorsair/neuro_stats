908b1420778b4ce9155e0e0a2ef05c1017d6de1b
cobaltCorsair <cobaltcorsair@yandex.ru>
2023-07-12 11:10:42 +0300

    added columns reading

---
+++ neuro_stats/read_columns.py
@@ -0,0 +1,36 @@
+import cv2
+import numpy as np
+
+# Загрузка изображения
+img = cv2.imread('../test.png', 0)
+
+# Бинаризация изображения
+_, binary_img = cv2.threshold(img, 150, 255, cv2.THRESH_BINARY_INV)
+
+# Создание структурирующего элемента для детектирования горизонтальных линий
+horizontal_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (19, 1))
+
+# Применение морфологической операции для выделения горизонтальных линий
+detected_lines = cv2.morphologyEx(binary_img, cv2.MORPH_OPEN, horizontal_kernel, iterations=2)
+
+# Удаление линий из изображения
+img_without_lines = cv2.subtract(binary_img, detected_lines)
+
+# Находим области с текстом
+dilated_img = cv2.dilate(img_without_lines, None, iterations=2)
+contours, _ = cv2.findContours(dilated_img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+img_color = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
+
+# Рисование контуров
+for cnt in contours:
+    x, y, w, h = cv2.boundingRect(cnt)
+
+    # Отсечение слишком маленьких контуров, которые могут быть шумом
+    if w * h > 100:
+        cv2.rectangle(img_color, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+# Отображение изображения
+cv2.imshow('image', img_color)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
---
+++ neuro_stats/test_mnist.py
@@ -0,0 +1,84 @@
+import matplotlib.pyplot as plt
+import torchvision
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision
+from PIL import Image
+import numpy as np
+
+# Load MNIST data
+transform = torchvision.transforms.Compose([
+    torchvision.transforms.ToTensor(),
+    torchvision.transforms.Normalize((0,), (128,)),
+])
+train = torchvision.datasets.MNIST('data', train=True, download=True, transform=transform)
+test = torchvision.datasets.MNIST('data', train=True, download=True, transform=transform)
+trainloader = torch.utils.data.DataLoader(train, shuffle=True, batch_size=100)
+
+fig, ax = plt.subplots(4, 4, sharex=True, sharey=True)
+for i in range(4):
+    for j in range(4):
+        ax[i][j].imshow(train.data[4 * i + j], cmap="gray")
+plt.show()
+
+print(train.data.shape, train.targets.shape)
+
+X_train = train.data.reshape(-1, 784).float() / 255.0
+y_train = train.targets
+
+loader = torch.utils.data.DataLoader(list(zip(X_train, y_train)), shuffle=True, batch_size=100)
+
+
+class Baseline(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.layer1 = nn.Linear(784, 784)
+        self.act1 = nn.ReLU()
+        self.layer2 = nn.Linear(784, 10)
+
+    def forward(self, x):
+        x = self.act1(self.layer1(x))
+        x = self.layer2(x)
+        return x
+
+if True:
+    model = Baseline()
+
+    optimizer = optim.SGD(model.parameters(), lr=0.01)
+    loss_fn = nn.CrossEntropyLoss()
+    loader = torch.utils.data.DataLoader(list(zip(X_train, y_train)), shuffle=True, batch_size=100)
+
+    n_epochs = 10
+    for epoch in range(n_epochs):
+        model.train()
+        for X_batch, y_batch in loader:
+            y_pred = model(X_batch)
+            loss = loss_fn(y_pred, y_batch)
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+    print(X_train.shape)
+
+    # Save model
+    torch.save(model.state_dict(), 'model.pth')
+
+
+# Load model
+
+model = Baseline()
+model.load_state_dict(torch.load('model.pth'))
+model.eval()
+
+p = Image.open("../test.png")
+px = np.array(p)
+px = px[472:472+28, 738:738+28, 0]
+plt.imshow(px)
+plt.show()
+
+t = torch.from_numpy(px)
+t2 = t.reshape(-1, 784).float() / 255.0
+y_pred = model(t2)
+print(y_pred)
+print(torch.max(y_pred, 1))
+print(torch.argmax(y_pred, 1))
