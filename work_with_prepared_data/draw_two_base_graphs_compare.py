3f80b6e287bbfab44f9af5ddc0b38b82c5b3119c
cobaltCorsair <cobaltcorsair@yandex.ru>
2023-10-10 19:00:13 +0300

    refactoring

--- work_with_prepared_data/draw_two_base_graphs_compare.py
+++ work_with_prepared_data/draw_two_base_graphs_compare.py
@@ -1,5 +1,4 @@
 import os
-
 import matplotlib.pyplot as plt
 from draw_base_grapfs import TumorDataVisualizer
 import numpy as np
@@ -7,42 +6,56 @@ import numpy as np
 
 class TumorDataComparator:
     def __init__(self, visualizer1: TumorDataVisualizer, visualizer2: TumorDataVisualizer):
+        """
+        Инициализатор класса сравнителя данных о опухолях.
+
+        Parameters:
+            visualizer1 (TumorDataVisualizer): Визуализатор данных первого эксперимента.
+            visualizer2 (TumorDataVisualizer): Визуализатор данных второго эксперимента.
+        """
         self.visualizer1 = visualizer1
         self.visualizer2 = visualizer2
 
     def save_plot(self, comparison_type: str):
+        """
+        Сохраняет текущий график в файл PNG.
+
+        Parameters:
+            comparison_type (str): Тип сравнения, используется в имени файла.
+        """
         # Извлечение имен файлов без расширения и пути
         file_name1 = os.path.splitext(os.path.basename(self.visualizer1.file_path))[0]
         file_name2 = os.path.splitext(os.path.basename(self.visualizer2.file_path))[0]
 
-        # Сборка окончательного имени файла
+        # Сборка окончательного имени файла и сохранение графика
         file_name = f"{comparison_type}_{file_name1}_vs_{file_name2}.png"
-
-        # Сохранение изображения
         plt.savefig(file_name, format='png', dpi=300)
         print(f"Plot saved as {file_name}")
 
     def normalize_time_data(self):
-        # Перевести временные метки в числовой формат и нормализовать их
+        """
+        Нормализует временные метки, приводя их к числовому формату и вычитая начальное время.
+        """
         self.visualizer1.time_data = [int(time) - int(self.visualizer1.time_data[0]) for time in
                                       self.visualizer1.time_data]
         self.visualizer2.time_data = [int(time) - int(self.visualizer2.time_data[0]) for time in
                                       self.visualizer2.time_data]
 
     def compare_tumor_volumes(self):
+        """
+        Сравнивает абсолютные объемы опухолей между двумя экспериментами и строит соответствующий график.
+        """
         # Нормализовать временные метки
         self.normalize_time_data()
 
+        # Инициализация графика и построение данных для каждого эксперимента
         plt.figure(figsize=(15, 8))
-
-        # Построение графиков для первого эксперимента
         for label, volumes in zip(self.visualizer1.rat_labels, self.visualizer1.tumor_volumes):
             plt.plot(self.visualizer1.time_data, volumes, marker='o', linestyle='-', label=f"Exp1: {label}")
-
-        # Построение графиков для второго эксперимента
         for label, volumes in zip(self.visualizer2.rat_labels, self.visualizer2.tumor_volumes):
             plt.plot(self.visualizer2.time_data, volumes, marker='o', linestyle='--', label=f"Exp2: {label}")
 
+        # Добавление меток, заголовка и сетки на график
         plt.title(
             f"Сравнение экспериментов \n\n Exp1: {', '.join(self.visualizer1.experiment_params)} vs Exp2: {', '.join(self.visualizer2.experiment_params)}",
             fontsize=16, y=1.02)
@@ -52,23 +65,26 @@ class TumorDataComparator:
         plt.grid(True)
         plt.legend(title="Метка крысы")
         plt.tight_layout()
+
+        # Сохранение графика
         self.save_plot("compare_tumor_volumes")
         plt.show()
 
     def compare_relative_tumor_volumes(self):
+        """
+        Сравнивает относительные объемы опухолей между двумя экспериментами и строит соответствующий график.
+        """
         # Нормализовать временные метки
         self.normalize_time_data()
 
+        # Инициализация графика и построение данных для каждого эксперимента
         plt.figure(figsize=(15, 8))
-
-        # Построение графиков относительных объемов для первого эксперимента
         for label, volumes in zip(self.visualizer1.rat_labels, self.visualizer1.get_relative_tumor_volumes()):
             plt.plot(self.visualizer1.time_data, volumes, marker='o', linestyle='-', label=f"Exp1: {label}")
-
-        # Построение графиков относительных объемов для второго эксперимента
         for label, volumes in zip(self.visualizer2.rat_labels, self.visualizer2.get_relative_tumor_volumes()):
             plt.plot(self.visualizer2.time_data, volumes, marker='o', linestyle='--', label=f"Exp2: {label}")
 
+        # Добавление меток, заголовка и сетки на график
         plt.title(
             f"Сравнение относительных объемов опухолей \n\n Exp1: {', '.join(self.visualizer1.experiment_params)} vs Exp2: {', '.join(self.visualizer2.experiment_params)}",
             fontsize=16, y=1.02)
@@ -78,6 +94,8 @@ class TumorDataComparator:
         plt.grid(True)
         plt.legend(title="Метка крысы")
         plt.tight_layout()
+
+        # Сохранение графика
         self.save_plot("compare_relative_tumor_volumes")
         plt.show()
 
